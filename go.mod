dc10ad929316570274eeda0edcdfa0d947d4d6a7
cyrus <710058301@qq.com>
2022-02-10 00:45:14 +0800

    add httpclient

--- go.mod
+++ go.mod
@@ -3,19 +3,17 @@ module github.com/cyruslo/library
 go 1.16
 
 require (
-	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
-	github.com/bilibili/kratos v0.3.2-0.20191127025250-43a13f6aae20
+	github.com/bilibili/kratos v0.3.3
+	github.com/cyruslo/util v0.0.4
 	github.com/denisenkom/go-mssqldb v0.12.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gogo/protobuf v1.3.1
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/golang/protobuf v1.3.2
-	github.com/google/uuid v1.1.1
+	github.com/google/uuid v1.3.0
 	github.com/influxdata/influxdb1-client v0.0.0-20190809212627-fc22c7df067e
-	github.com/pkg/errors v0.8.1
-	github.com/shirou/gopsutil v2.19.11+incompatible // indirect
+	github.com/pkg/errors v0.9.1
 	github.com/zhenjl/cityhash v0.0.0-20131128155616-cdd6a94144ab
 	golang.org/x/net v0.0.0-20210610132358-84b48f89b13b
-	google.golang.org/genproto v0.0.0-20191216205247-b31c10ee225f // indirect
 	google.golang.org/grpc v1.24.0
 )
---
+++ pkg/httpclient/http_client.go
@@ -0,0 +1,131 @@
+
+package http_client
+
+import (
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"github.com/cyruslo/library/pkg/beego/httplib"
+	LOGGER "github.com/cyruslo/util/logger"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+var (
+	clientCACert []byte
+)
+///////////////////////////////////
+//http_client采用beego框架的httplib库,需要新增方法参考:https://beego.me/docs/module/httplib.md
+
+//get请求str
+func HttpGetStr(host string, request string, content string) string {
+	url := fmt.Sprintf("%s/%s?%s", host, request, content)
+	
+	log.Printf("url:%s.", url)
+	
+	rep := httplib.Get(url)	
+	
+	str, err := rep.String()
+	
+	if err != nil {
+		log.Printf("httplib err")
+		
+		fmt.Println(err)
+		return ""
+	}
+
+//	fmt.Println(str)
+	return str
+}
+
+func HttpsGetStr(host, request, content string) string {
+	pool := x509.NewCertPool()
+ 
+    pool.AppendCertsFromPEM(clientCACert)
+    tr := &http.Transport{
+        TLSClientConfig: &tls.Config{RootCAs: pool},
+    }
+	client := &http.Client{Transport: tr}
+
+	url := fmt.Sprintf("%s/%s?%s", host, request, content)
+	log.Printf("url:%s.", url)
+	
+    resp, err := client.Get(url)
+    if err != nil {
+        fmt.Println("Get error:", err)
+        return ""
+    }
+    defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	res := string(body)
+	//fmt.Println(res)
+	return res
+}
+/*
+//get请求json
+func HttpGetJson(host string, request string, content string) interface{} {
+	url := fmt.Sprintf("%s/%s?%s", host, request, content)
+
+	rep := httplib.Get(url)
+	str, err := rep.String()
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	fmt.Println(str)
+	var result interface{}
+	return rep.ToJSON(&result)
+}*/
+
+//post请求string
+func HttpPostStr(host string, request string, content string, values map[string]string) string {
+	url := fmt.Sprintf("%s/%s?%s", host, request, content)
+
+	rep := httplib.Post(url)
+	for key := range values {
+		rep.Param(key, values[key])
+	}
+	
+	str, err := rep.String()
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}	
+
+//	fmt.Println(str)
+	return str
+}
+
+//post请求xml		
+func HttpPostXml(url string, output []byte)  {
+
+    var jsonStr = []byte(output)
+ 
+    req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+    // req.Header.Set("X-Custom-Header", "myvalue")
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+		fmt.Println("client.Do err:%v.", err)
+		return
+    }
+    defer resp.Body.Close()
+    body, _ := ioutil.ReadAll(resp.Body)
+    LOGGER.Info("response Body:", string(body))
+}
+
+func OnRead(caCertPath string) {
+
+    caCrt, err := ioutil.ReadFile(caCertPath)
+    if err != nil {
+        fmt.Println("ReadFile err:", err)
+        return
+	}
+	
+	clientCACert = caCrt
+}
